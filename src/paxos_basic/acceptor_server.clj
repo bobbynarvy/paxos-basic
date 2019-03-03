5f443bfe98a767eff792c59ae78467ed0fb624ce
Robert Narvaez <narvaez.rm@gmail.com>
2019-03-03 15:54:44 +0000

    outline steps in acceptor server

--- src/paxos_basic/acceptor_server.clj
+++ src/paxos_basic/acceptor_server.clj
@@ -1,13 +1,22 @@
 (ns paxos-basic.acceptor-server
-  (require [paxos-basic.socket-server :as server]))
+  (require [paxos-basic.socket-server :as server]
+           [paxos-basic.message :as message]))
+
+(defn request-handler
+  "Gets the handler based on the
+  type of request"
+  [request]
+  ())
 
 (defn acceptor-handler
-  [data]
-  ;; Parse the data into a request
-  ;; Determine the type of request
-  ;; Create the response based on the request type
-  ;; Convert and return the response to a string
-  )
+  "Returns a response from an acceptor
+  based on a request from a proposer"
+  [str-data]
+  (let [request (message/str->map str-data)         ;; Parse the data into a request
+        handler (request-handler request)           ;; Get the handler based on the type of request
+        response (handler request)                  ;; Create the acceptor response
+        str-response (message/map->str response)]   ;; Convert and return the response to a string
+    str-response))
 
 (defn serve
   "Start a process that listens to
